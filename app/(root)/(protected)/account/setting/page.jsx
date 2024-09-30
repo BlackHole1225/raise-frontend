a917304c0611c92569cd36399dd4161027648b07
monolab825 <p.smiles777@gmail.com>
2024-10-01 08:45:43 +0900

    update account setting page modal

--- app/(root)/(protected)/account/setting/page.jsx
+++ app/(root)/(protected)/account/setting/page.jsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
 import { Avatar } from '@nextui-org/avatar';
+import { Input, Textarea } from '@nextui-org/input';
 import { FaCheck } from "react-icons/fa6";
 import { IoMdClose } from "react-icons/io";
+import { MdOutlineVisibility, MdOutlineVisibilityOff } from "react-icons/md";
 import DragDropUpload from '@/components/ui/dragDropUpload';
 
 const Setting = () => {
@@ -19,48 +21,9 @@ const Setting = () => {
     setOpenModal(null);
   };
 
-  const uploadFile = async (files) => {
-    const formData = new FormData();
+  const [isVisible, setIsVisible] = React.useState(false);
 
-    // If multiple files, append them
-    if (Array.isArray(files)) {
-      files.forEach((file, index) => formData.append('file_' + index, file));
-    } else {
-      formData.append('files', files); // Single file
-    }
-
-    try {
-      const response = await axios.post(`${FSERVER_IP}/api/file/upload`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-
-      // Ensure the response contains the uploaded file information
-      const uploadedFiles = response.data.uploaded;
-      console.log('Uploaded Files:', uploadedFiles);
-
-      if (uploadedFiles != undefined) {
-        // Map the file IDs
-        const fileIds = uploadedFiles.map((file) => file._id);
-        return fileIds;
-      }
-    } catch (error) {
-      console.error('Error uploading files:', error);
-      throw error;
-    }
-  };
-  // Function to handle the campaign image upload
-  const handleCamImg = (img) => {
-    setCampaignImage(img);
-    if (typeof window !== 'undefined' && img != undefined) {
-      const campaignImgReader = new FileReader();
-      campaignImgReader.onloadend = function () {
-        setCamImgFile(campaignImgReader.result);
-      };
-      campaignImgReader.readAsDataURL(img); // Trigger onloadend
-    }
-  };
+  const toggleVisibility = () => setIsVisible(!isVisible);
 
   return (
     <div className="pt-20 pb-[232px]">
@@ -128,7 +91,7 @@ const Setting = () => {
 
       {openModal === 1 && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="relative bg-brand-lemon-yellow px-9 pt-10 pb-[50px] rounded-lg shadow-lg w-1/3 text-brand-dark">
+          <div className="relative bg-brand-lemon-yellow px-9 pt-10 pb-[50px] rounded-lg shadow-lg w-1/3 text-brand-olive-green">
             <h3 className="text-3xl font-bold mb-4">EDIT PROFILE</h3>
             <button
               onClick={handleCloseModal}
@@ -143,6 +106,32 @@ const Setting = () => {
               isMultiple={false}
               label=""
             />
+            <Input
+              size="lg"
+              variant="bordered"
+              label="Name"
+              radius="sm"
+              placeholder=""
+              classNames={{
+                inputWrapper:
+                  'border border-brand-dark hover:border-brand-dark data-[hover=true]:border-brand-dark mt-4 h-full',
+                input: 'py-1',
+                label: 'font-bold text-xl'
+              }}
+            />
+            <Input
+              size="lg"
+              variant="bordered"
+              label="Address"
+              radius="sm"
+              placeholder=""
+              classNames={{
+                inputWrapper:
+                  'border border-brand-dark hover:border-brand-dark data-[hover=true]:border-brand-dark mt-4 h-full',
+                input: 'py-1',
+                label: 'font-bold text-xl'
+              }}
+            />
             <div className='flex items-center gap-2 mt-[58px]'>
               <button
                 onClick={handleCloseModal}
@@ -151,9 +140,7 @@ const Setting = () => {
                 <IoMdClose size={16} />
                 Close
               </button>
-              <button
-                onClick={handleCloseModal}
-                className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
+              <button className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
               >
                 <FaCheck size={16} />
                 Save
@@ -164,7 +151,7 @@ const Setting = () => {
       )}
       {openModal === 2 && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="relative bg-brand-lemon-yellow px-9 pt-10 pb-[50px] rounded-lg shadow-lg w-1/3 text-brand-dark">
+          <div className="relative bg-brand-lemon-yellow px-9 pt-10 pb-[50px] rounded-lg shadow-lg w-1/3 text-brand-olive-green">
             <h3 className="text-3xl font-bold mb-4">CHANGE PASSWORD</h3>
             <button
               onClick={handleCloseModal}
@@ -172,8 +159,76 @@ const Setting = () => {
             >
               &times;
             </button>
-            <p className="mb-4">Change your password here.</p>
-            <div className='flex items-center gap-2 mt-[58px]'>
+            <p className="mb-6 text-lg font-bold">Enter the previous password and then the new password to change your account password.</p>
+            <div className='mt-[60px]'>
+              <Input
+                size="lg"
+                variant="bordered"
+                label="Previous Password"
+                radius="sm"
+                classNames={{
+                  inputWrapper:
+                    'border border-brand-dark hover:border-brand-dark data-[hover=true]:border-brand-dark mt-4 h-full',
+                  input: 'py-1',
+                  label: 'font-bold text-xl'
+                }}
+                endContent={
+                  <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+                    {isVisible ? (
+                      <MdOutlineVisibilityOff className="text-2xl pointer-events-none" />
+                    ) : (
+                      <MdOutlineVisibility className="text-2xl pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isVisible ? "text" : "password"}
+              />
+              <Input
+                size="lg"
+                variant="bordered"
+                label="New Password"
+                radius="sm"
+                classNames={{
+                  inputWrapper:
+                    'border border-brand-dark hover:border-brand-dark data-[hover=true]:border-brand-dark mt-4 h-full',
+                  input: 'py-1',
+                  label: 'font-bold text-xl'
+                }}
+                endContent={
+                  <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+                    {isVisible ? (
+                      <MdOutlineVisibilityOff className="text-2xl pointer-events-none" />
+                    ) : (
+                      <MdOutlineVisibility className="text-2xl pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isVisible ? "text" : "password"}
+              />
+              <Input
+                size="lg"
+                variant="bordered"
+                label="New Password(Again)"
+                radius="sm"
+                classNames={{
+                  inputWrapper:
+                    'border border-brand-dark hover:border-brand-dark data-[hover=true]:border-brand-dark mt-4 h-full',
+                  input: 'py-1',
+                  label: 'font-bold text-xl'
+                }}
+                endContent={
+                  <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+                    {isVisible ? (
+                      <MdOutlineVisibilityOff className="text-2xl pointer-events-none" />
+                    ) : (
+                      <MdOutlineVisibility className="text-2xl pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isVisible ? "text" : "password"}
+              />
+            </div>
+            <div className='flex items-center gap-2 mt-[129px]'>
               <button
                 onClick={handleCloseModal}
                 className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
@@ -181,9 +236,7 @@ const Setting = () => {
                 <IoMdClose size={16} />
                 Close
               </button>
-              <button
-                onClick={handleCloseModal}
-                className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
+              <button className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
               >
                 <FaCheck size={16} />
                 Save
@@ -194,7 +247,7 @@ const Setting = () => {
       )}
       {openModal === 3 && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="relative bg-brand-lemon-yellow px-9 pt-10 pb-[50px] rounded-lg shadow-lg w-1/3 text-brand-dark">
+          <div className="relative bg-brand-lemon-yellow px-9 pt-10 pb-[50px] rounded-lg shadow-lg w-1/3 text-brand-olive-green">
             <h3 className="text-3xl font-bold mb-4">CHANGE EMAIL</h3>
             <button
               onClick={handleCloseModal}
@@ -202,31 +255,34 @@ const Setting = () => {
             >
               &times;
             </button>
-            <p className="mb-4">Change your Email here.</p>
-
-            <div className='flex items-center gap-2'>
-            </div>          <div className='flex items-center gap-2 mt-[58px]'>
-              <button
-                onClick={handleCloseModal}
-                className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
-              >
-                <IoMdClose size={16} />
-                Close
-              </button>
-              <button
-                onClick={handleCloseModal}
-                className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
-              >
-                <FaCheck size={16} />
-                Save
-              </button>
+            <p className="mb-6 text-lg font-bold">Enter your new email address and then verify your email by adding the otp sent on your email address to change your account email.</p>
+            <div className='mt-9'>
+              <Input
+                size="lg"
+                variant="bordered"
+                label="New Email Address"
+                radius="sm"
+                placeholder=""
+                classNames={{
+                  inputWrapper:
+                    'border border-brand-dark hover:border-brand-dark data-[hover=true]:border-brand-dark h-full',
+                  input: 'py-1',
+                  label: 'font-bold text-xl'
+                }}
+              />
             </div>
+            <button
+              onClick={handleCloseModal}
+              className="mt-6 w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green hover:text-red-500 hover:border-red-500"
+            >
+              Send OTP
+            </button>
           </div>
         </div>
       )}
       {openModal === 4 && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="relative bg-brand-lemon-yellow px-9 pt-10 pb-[50px] rounded-lg shadow-lg w-1/3 text-brand-dark">
+          <div className="relative bg-brand-lemon-yellow px-9 pt-10 pb-[50px] rounded-lg shadow-lg w-1/3 text-brand-olive-green">
             <h3 className="text-3xl font-bold mb-4">CHANGE PHONE NUMBER</h3>
             <button
               onClick={handleCloseModal}
@@ -245,9 +301,7 @@ const Setting = () => {
                 <IoMdClose size={16} />
                 Close
               </button>
-              <button
-                onClick={handleCloseModal}
-                className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
+              <button className="w-fit px-[18px] py-[10px] text-sm font-bold border border-brand-olive-green rounded-full text-brand-olive-green flex items-center gap-1 hover:text-red-500 hover:border-red-500"
               >
                 <FaCheck size={16} />
                 Save
--- components/ui/dragDropUpload.jsx
+++ components/ui/dragDropUpload.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Cloud, File } from 'lucide-react';
+import { SlCloudUpload } from "react-icons/sl";
 
 const DragDropUpload = ({ acceptedFormats, isMultiple, label, onChange }) => {
   const [files, setFiles] = useState([]);
@@ -32,23 +33,23 @@ const DragDropUpload = ({ acceptedFormats, isMultiple, label, onChange }) => {
     <div className="w-full mx-auto">
       <div
         {...getRootProps()}
-        className={`border-2 border-dashed rounded-lg p-8 text-center cursor-pointer transition-colors
-          ${isDragActive ? 'border-blue-500 bg-blue-50' : 'border-gray-300 hover:border-gray-400'}`}
+        className={`border-2 border-dashed rounded-lg p-14 text-center cursor-pointer transition-colors text-brand-olive-green
+          ${isDragActive ? 'border-blue-500 bg-blue-50' : 'border-brand-olive-green hover:border-gray-400'}`}
       >
         <input {...getInputProps()} />
-        <Cloud className="mx-auto h-12 w-12 text-gray-400" />
-        <h2 className="mt-2 text-lg font-semibold text-gray-700">{label}</h2>
-        <p className="mt-1 text-sm text-gray-500">Drag & Drop or Browse</p>
+        <SlCloudUpload size={40} className='mx-auto' />
+        <h2 className="mt-2 text-lg font-semibold ">{label}</h2>
         {files.length > 0 && (
           <div className="mt-4">
             {files.map((file, index) => (
-              <div key={index} className="flex items-center text-sm text-gray-600">
+              <div key={index} className="flex items-center justify-center text-xl font-bold ">
                 <File className="mr-2 h-4 w-4" />
-                {file.name}
+                {file.name} is selected
               </div>
             ))}
           </div>
         )}
+        <p className="mt-1 text-sm text-gray-600">Drag & Drop or Browse</p>
       </div>
     </div>
   );
