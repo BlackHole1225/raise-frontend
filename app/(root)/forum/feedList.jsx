c64441f768eb8ea59dce0a33216c13e251e874d3
blackhole1225 <blackhole1225.dev@gmail.com>
2024-10-02 15:34:48 +0900

    fix:forum-comments function

--- app/(root)/forum/feedList.jsx
+++ app/(root)/forum/feedList.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 
 
-const FeedList = () => {
+const FeedList = ({feedfontSize}) => {
   const hasData = true;
 
     return (
@@ -16,6 +16,7 @@ const FeedList = () => {
             comments={10}
             votes={5}
             accessTime={10}
+            fontSize={feedfontSize}
             imageUrl="https://s3-alpha-sig.figma.com/img/55a2/582c/74201026b0fe42f528e1e3709ed824b7?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WFCJt36rlhoxY4vHrBoECck4ErKYZLBJBLhOF2t02Gg3vVHX1fOSEW9tRfUpj5T3rP7lFe6Ms1lRGCssdIUYCzVE85hrs~tsoZcBYEAilsSPwlgZUk45aMfs5KROGKFxjwHVqIANFz~pOThvT6zQojzJGQ1MDxPcA6zAm-Yb~TxlfbG9GGVqV2xliF69l~VTRwioRy9FVeAB7bRw7QyBLHcvAh~sJifKxnUSXzLB7EnMdGxiM8Mo7WwQxHq~wPRbKWN36MHkEI9524B~1lu5YuKLIfJ6qB4QKj2kTHvda0zVw-37Hi5GG2f44CTdXxszAsp6SxgA9pF31Tsa~3Hf0A__"
             reporterPhoto="https://s3-alpha-sig.figma.com/img/8356/7f57/7a03ba13dd8974f6b817895895bc8831?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KP2pgcg4S~3p2-wjbg46I~Abxyy4kq9t3G5uMpMjEcS~kUuiYmJEi5TnOgD7TO4DiD80YFV1B9xI1eRDOytA368yRxoNOGWgzn9gkdRXsGKj4JxdEoFkplVRvKRoHwmbWruAl1r6vzGkHgwjqQ5JGXJuY-19UVPg8q10GL9OkAjYia6KMtS8-I2r-z4iRfrKl2BORJ7aOe7HsziHoZxYOCZiDxKlpSlZrFcOoFaC2jxWzy8WHMEnKrM0j48ArHguEof5vGW~bPfBFw~kvrqhvhzFfovFIGk-7Kxttzm9erMX38AwtDA7j98rXT3Jd7mt0APGwRS-HuOu6U8DrOP0sg__"
 
@@ -25,6 +26,8 @@ const FeedList = () => {
             comments={10}
             votes={5}
             accessTime={10}
+            fontSize={feedfontSize}
+
             imageUrl="https://s3-alpha-sig.figma.com/img/55a2/582c/74201026b0fe42f528e1e3709ed824b7?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WFCJt36rlhoxY4vHrBoECck4ErKYZLBJBLhOF2t02Gg3vVHX1fOSEW9tRfUpj5T3rP7lFe6Ms1lRGCssdIUYCzVE85hrs~tsoZcBYEAilsSPwlgZUk45aMfs5KROGKFxjwHVqIANFz~pOThvT6zQojzJGQ1MDxPcA6zAm-Yb~TxlfbG9GGVqV2xliF69l~VTRwioRy9FVeAB7bRw7QyBLHcvAh~sJifKxnUSXzLB7EnMdGxiM8Mo7WwQxHq~wPRbKWN36MHkEI9524B~1lu5YuKLIfJ6qB4QKj2kTHvda0zVw-37Hi5GG2f44CTdXxszAsp6SxgA9pF31Tsa~3Hf0A__"
             reporterPhoto="https://s3-alpha-sig.figma.com/img/8356/7f57/7a03ba13dd8974f6b817895895bc8831?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KP2pgcg4S~3p2-wjbg46I~Abxyy4kq9t3G5uMpMjEcS~kUuiYmJEi5TnOgD7TO4DiD80YFV1B9xI1eRDOytA368yRxoNOGWgzn9gkdRXsGKj4JxdEoFkplVRvKRoHwmbWruAl1r6vzGkHgwjqQ5JGXJuY-19UVPg8q10GL9OkAjYia6KMtS8-I2r-z4iRfrKl2BORJ7aOe7HsziHoZxYOCZiDxKlpSlZrFcOoFaC2jxWzy8WHMEnKrM0j48ArHguEof5vGW~bPfBFw~kvrqhvhzFfovFIGk-7Kxttzm9erMX38AwtDA7j98rXT3Jd7mt0APGwRS-HuOu6U8DrOP0sg__"
 
@@ -34,6 +37,8 @@ const FeedList = () => {
             comments={10}
             votes={5}
             accessTime={10}
+            fontSize={feedfontSize}
+            reporterName='Diana Roobert'
             imageUrl="https://s3-alpha-sig.figma.com/img/55a2/582c/74201026b0fe42f528e1e3709ed824b7?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WFCJt36rlhoxY4vHrBoECck4ErKYZLBJBLhOF2t02Gg3vVHX1fOSEW9tRfUpj5T3rP7lFe6Ms1lRGCssdIUYCzVE85hrs~tsoZcBYEAilsSPwlgZUk45aMfs5KROGKFxjwHVqIANFz~pOThvT6zQojzJGQ1MDxPcA6zAm-Yb~TxlfbG9GGVqV2xliF69l~VTRwioRy9FVeAB7bRw7QyBLHcvAh~sJifKxnUSXzLB7EnMdGxiM8Mo7WwQxHq~wPRbKWN36MHkEI9524B~1lu5YuKLIfJ6qB4QKj2kTHvda0zVw-37Hi5GG2f44CTdXxszAsp6SxgA9pF31Tsa~3Hf0A__"
             reporterPhoto="https://s3-alpha-sig.figma.com/img/8356/7f57/7a03ba13dd8974f6b817895895bc8831?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KP2pgcg4S~3p2-wjbg46I~Abxyy4kq9t3G5uMpMjEcS~kUuiYmJEi5TnOgD7TO4DiD80YFV1B9xI1eRDOytA368yRxoNOGWgzn9gkdRXsGKj4JxdEoFkplVRvKRoHwmbWruAl1r6vzGkHgwjqQ5JGXJuY-19UVPg8q10GL9OkAjYia6KMtS8-I2r-z4iRfrKl2BORJ7aOe7HsziHoZxYOCZiDxKlpSlZrFcOoFaC2jxWzy8WHMEnKrM0j48ArHguEof5vGW~bPfBFw~kvrqhvhzFfovFIGk-7Kxttzm9erMX38AwtDA7j98rXT3Jd7mt0APGwRS-HuOu6U8DrOP0sg__"
 
@@ -50,11 +55,11 @@ const FeedList = () => {
        </>
     );
 };
-const FeedItem = ({ title, comments, votes, imageUrl, accessTime, reporterPhoto, reporterName }) => (
+const FeedItem = ({ title, comments, votes, imageUrl, fontSize, accessTime, reporterPhoto, reporterName }) => (
     <article className="flex">
         <img src={imageUrl} alt={imageUrl} className="w-[260px] h-[205px] object-cover" />
         <div className="flex flex-col bg-[#FAFF7D] pt-[30px] px-[22px]">
-            <h2 className="text-[32px] font-bold tracking-wider uppercase text-brand-dark font-heading">
+            <h2 className=" font-bold tracking-wider uppercase text-brand-olive-green font-heading" style={{fontSize:fontSize}}>
                 {title}
             </h2>
             <div className='flex gap-1 pb-2'>
@@ -64,7 +69,7 @@ const FeedItem = ({ title, comments, votes, imageUrl, accessTime, reporterPhoto,
             </div>
             <div className='flex gap-2'>
                 <img src={reporterPhoto} alt={reporterPhoto} className="w-[34px] h-[28px] object-cover rounded-lg" />
-                <p className="text-base font-bold tracking-wider text-brand-dark">Diana Roobert</p>
+                <p className="text-base font-bold tracking-wider text-brand-dark">{reporterName}</p>
             </div>
         </div>
 
