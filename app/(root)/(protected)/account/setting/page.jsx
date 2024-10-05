7102d032222a49bd11b0065edf2370d718134300
blackhole1225 <blackhole1225.dev@gmail.com>
2024-10-05 17:38:09 +0900

    fix:conflit

--- app/(root)/(protected)/account/setting/page.jsx
+++ app/(root)/(protected)/account/setting/page.jsx
@@ -12,6 +12,7 @@ import { MdOutlineVisibility, MdOutlineVisibilityOff } from "react-icons/md";
 import DragDropUpload from '@/components/ui/dragDropUpload';
 import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/otpInput';
 import { SERVER_LOCAL_IP } from '@/utils/constants';
+import {useRouter} from "next/navigation"
 import { notifySuccess, notifyError } from '@/components/notification';
 
 function UseClientSideStorage(key, defaultValue) {
