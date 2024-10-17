65be4fdfbe387384986a6ae227ae56d295e053d4
monolab825 <p.smiles777@gmail.com>
2024-10-18 03:53:27 +0900

    update constants

--- utils/constants.js
+++ utils/constants.js
@@ -6,5 +6,6 @@ export const LOCALNET = 'http://localhost:8899';
 export const DEVNET = 'https://api.devnet.solana.com';
 export const TESTNET = 'https://api.testnet.solana.com';
 export const MAINNET = 'https://api.mainnet-beta.solana.com';
-export const SERVER_LOCAL_IP = 'http://62.3.6.179:5005';
+export const SERVER_LOCAL_IP = 'https://raise-backend-new.vercel.app';
+// export const SERVER_LOCAL_IP = 'http://62.3.6.179:5005';
 export const GOOGLE_CLIENT_ID = '1090624351432-qlmvjt3n41l443r1mrom6frnn2lm3nrp.apps.googleusercontent.com';
