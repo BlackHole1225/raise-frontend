fa0c3821f7567bc1c36367c30aa8328fadfc528c
monolab825 <p.smiles777@gmail.com>
2024-10-18 17:07:33 +0900

    udpate register images

--- app/(authscreen)/(create-account)/layout.jsx
+++ app/(authscreen)/(create-account)/layout.jsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import Image from 'next/image';
 
 // Define the Layout component
 export default function Layout({ children }) {
@@ -12,10 +11,8 @@ export default function Layout({ children }) {
         <div className="flex flex-col w-[35%] max-md:ml-0 max-lg:w-full">
           <div className="flex flex-col self-stretch my-auto w-full text-xl font-bold text-zinc-800 max-md:mt-10 max-md:max-w-full">
             <div className="flex gap-5 self-start text-5xl whitespace-nowrap max-md:text-4xl">
-              <Image
+              <img
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/2dc00ade95da09b368c1fef8bf0b3dc9966c324e821b245532ac1b2f21e73e3d?placeholderIfAbsent=true&apiKey=766be46e9945400fb0d82367510acded"
-                width={60}
-                height={60}
                 className="object-contain shrink-0 rounded-lg"
                 alt="Raise logo"
               />
@@ -26,10 +23,8 @@ export default function Layout({ children }) {
         </div>
         <div className="flex flex-col lg:ml-5 w-[65%] max-md:ml-0 max-lg:w-full h-full">
           <div className="flex overflow-hidden relative flex-col items-end grow text-2xl font-bold text-amber-50 rounded-3xl max-md:max-w-full h-full">
-            <Image
+            <img
               src="https://cdn.builder.io/api/v1/image/assets/TEMP/a2846126ea60dff5330f24631ee622ef87f7eb9be682a41444620bdd1e674324?placeholderIfAbsent=true&apiKey=766be46e9945400fb0d82367510acded"
-              layout="fill"
-              objectFit="cover"
               alt="Background image"
             />
             <div className="min-h-[450px] flex overflow-hidden relative flex-col px-11 pb-11 rounded-3xl max-md:px-5 max-md:max-w-full h-full justify-end">
@@ -38,10 +33,8 @@ export default function Layout({ children }) {
                 powerful Solona based backend system. Verify your campaign and get started raising
                 funds.
               </div>
-              <Image
+              <img
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/22c8ad87f4b256492c05fa36c78c87c98c6bf82f7f35eba404a7872b4e00ca7a?placeholderIfAbsent=true&apiKey=766be46e9945400fb0d82367510acded"
-                width={50}
-                height={10}
                 className="object-contain self-center mt-7 rounded-lg"
                 alt="Decorative element"
               />
